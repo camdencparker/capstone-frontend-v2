3addc1dbec58723d648e83bf274fb506845a0ea0
Camden <camdencparker@gmail.com>
2022-03-23 10:40:18 -0700

    theme implemented for user signUp

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -25,6 +25,52 @@ export default {
 </script>
 
 <template>
+  <div class="login-register-wrapper">
+    <div class="login-register-tab-list nav">
+      <a class="active" data-bs-toggle="tab" href="#lg1">
+        <h4>Sign Up</h4>
+      </a>
+    </div>
+    <div class="tab-content">
+      <div id="lg1" class="tab-pane active">
+        <div class="login-form-container">
+          <div class="login-register-form">
+            <form v-on:submit.prevent="submit()">
+              <h1>Sign Up Today!</h1>
+              <ul>
+                <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+              </ul>
+              <div>
+                <label>Name:</label>
+                <input type="text" v-model="newUserParams.name" />
+              </div>
+              <div>
+                <label>Email:</label>
+                <input type="email" v-model="newUserParams.email" />
+              </div>
+              <div>
+                <label>Phone:</label>
+                <input type="phone" v-model="newUserParams.phone" />
+              </div>
+              <div>
+                <label>Location:</label>
+                <input type="location" v-model="newUserParams.location" />
+              </div>
+              <div>
+                <label>Password:</label>
+                <input type="password" v-model="newUserParams.password" />
+              </div>
+              <div>
+                <label>Password confirmation:</label>
+                <input type="password" v-model="newUserParams.password_confirmation" />
+              </div>
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   <div class="signup">
     <form v-on:submit.prevent="submit()">
       <h1>SignUp Today!</h1>
