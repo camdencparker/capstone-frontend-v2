346db2957d19526a706af360e64cacc264690052
Camden <camdencparker@gmail.com>
2022-03-23 10:16:13 -0700

    listingsIndex theme implemented

--- src/views/ListingsIndex.vue
+++ src/views/ListingsIndex.vue
@@ -29,7 +29,50 @@ export default {
 </script>
 
 <template>
-  <div class="home">
+  <div class="Blog-area pt-100 pb-100 blog-no-sidebar">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-12" v-for="listing in filteredListings" v-bind:key="listing.id">
+              <div class="blog-wrap-2 mb-30">
+                <div class="blog-img-2">
+                  <router-link v-bind:to="`/listings/${listing.id}`">
+                    <a><img v-bind:src="listing.image_url" v-bind:alt="listing.brand" /></a>
+                  </router-link>
+                </div>
+                <div class="blog-content-2">
+                  <div class="blog-meta-2">
+                    <ul>
+                      <li>{{ listing.location }}</li>
+                      <li>
+                        <a href="#">
+                          <i class="fa fa-comments-o"></i>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <h4>
+                    <router-link v-bind:to="`/listings/${listing.id}`">
+                      <a>{{ listing.brand }} {{ listing.model }}</a>
+                    </router-link>
+                  </h4>
+                  <div class="blog-share-comment">
+                    <div class="blog-btn-2">
+                      <router-link v-bind:to="`/listings/${listing.id}`">
+                        <a href="blog-details.html">To Listing</a>
+                      </router-link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- <div class="home">
     <h1>All Listings</h1>
     <h2>Search by Brand:</h2>
     <input type="text" v-model="brandFilter" placeholder="Search by Brand" />
@@ -40,5 +83,5 @@ export default {
       </router-link>
       <p>Model: {{ listing.model }}</p>
     </div>
-  </div>
+  </div> -->
 </template>
