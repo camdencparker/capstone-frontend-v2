f79c5773b0320b4290fcf33a220dd7498160d68d
Camden <camdencparker@gmail.com>
2022-03-23 17:28:54 -0700

    creates landing page for web app

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,87 +1,30 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,
-      <br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>
-      .
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-        >
-          babel
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-        >
-          router
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-        >
-          eslint
-        </a>
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li>
-        <a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a>
-      </li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="slider-area">
+    <div class="slider-active owl-carousel nav-style-1 owl-dot-none">
+      <div
+        class="single-slider-2 slider-height-20 d-flex align-items-center slider-height-res bg-img"
+        style="
+          background-image: url(https://cdn.hiconsumption.com/wp-content/uploads/2018/04/Best-Motorcycle-Roads.jpg);
+        "
+      >
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <div class="slider-content-20 slider-animated-1">
+                <br />
+                <br />
+                <br />
+                <h3 style="color: darkgoldenrod" class="animated">Want The Freedom Two Wheels Brings?</h3>
+                <br />
+                <p style="color: darkgoldenrod" class="animated">We Only Regret The Rides We Don't Take</p>
+                <div class="slider-btn slider-btn-round btn-hover">
+                  <a class="animated" href="/listings">FIND YOUR BIKE HERE</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
-};
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="slider-area">
+    <div class="slider-active owl-carousel nav-style-1 owl-dot-none">
+      <div
+        class="single-slider-2 slider-height-20 d-flex align-items-center slider-height-res bg-img"
+        style="
+          background-image: url(https://cdn.hiconsumption.com/wp-content/uploads/2018/04/Best-Motorcycle-Roads.jpg);
+        "
+      >
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <div class="slider-content-20 slider-animated-1">
+                <br />
+                <br />
+                <br />
+                <h3 style="color: darkgoldenrod" class="animated">Want The Freedom Two Wheels Brings?</h3>
+                <br />
+                <p style="color: darkgoldenrod" class="animated">We Only Regret The Rides We Don't Take</p>
+                <div class="slider-btn slider-btn-round btn-hover">
+                  <a class="animated" href="/listings">FIND YOUR BIKE HERE</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
